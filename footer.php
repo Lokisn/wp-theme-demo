547dc2c1da652abec6bf6792c3db9779d6e991cf
Lokisn <2035137@cgmatane.qc.ca>
2022-02-07 16:34:54 -0500

    Add theme files

---
+++ footer.php
@@ -0,0 +1,9 @@
+</main>
+    <footer class="flex v-aligne">
+        <div class="conteneur">
+            <p><?php bloginfo( 'name' )?>, tous droits réservés &copy<?php echo(date("Y/M")) ?></p>
+        </div>
+    </footer>
+    <?php wp_footer(  )?>
+</body>
+</html>
