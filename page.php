6d3aed3c661cd6590e86dbd6219c9326598287a0
Lokisn <2035137@cgmatane.qc.ca>
2022-02-07 16:58:02 -0500

    Add page template

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header()?>
+
+    <?php if(have_posts( )) : ?>
+    <?php while(have_posts( )) : the_post(  ); ?>
+
+        <article class="flex">
+            <!-- Le contenu de l'article -->
+            <div class="texte-article">
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content( );?></p>
+            </div>
+        </article>
+
+        <?php endwhile;?>
+        <?php endif; ?>
+
+<?php get_footer( ) ?>
--- single.php
+++ single.php
@@ -1,7 +1,5 @@
 <?php get_header()?>
 
-
-
     <h2><?php the_title(); ?></h2>
     <div class="flex">
         <section class="trois-quart">
